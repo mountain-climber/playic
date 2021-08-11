eefe6085efb47ee86498020c5dd38a9334fa7bfa
mountain-climber <doublem.jquery@gmail.com>
2021-08-11 14:18:07 +0300

    seveth

--- app/js/main.js
+++ app/js/main.js
@@ -3,5 +3,9 @@ $(function () {
   $('.header__btn').on('click', function () {
     $('.menu').toggleClass('menu--open');
   });
+
+  if ($(window).width() < 993) {
+    // $('.users__content-title').appendTo($('.container'));
+  }
   
 });
