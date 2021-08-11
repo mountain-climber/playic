4d7b8c05ddcf1a191d741fcb2da5df8bb36dd793
mountain-climber <doublem.jquery@gmail.com>
2021-08-11 22:38:11 +0300

    nineth

--- app/js/main.js
+++ app/js/main.js
@@ -3,9 +3,5 @@ $(function () {
   $('.header__btn').on('click', function () {
     $('.menu').toggleClass('menu--open');
   });
-
-  if ($(window).width() < 993) {
-    // $('.users__content-title').appendTo($('.container'));
-  }
   
 });
