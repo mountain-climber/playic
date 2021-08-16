6bdd825e37046153024b81494267d6487b838543
mountain-climber <doublem.jquery@gmail.com>
2021-08-16 14:09:45 +0300

    tenth

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,24 @@
 $(function () {
 
-  $('.header__btn').on('click', function () {
+  $('.header__btn, .menu a').on('click', function () {
     $('.menu').toggleClass('menu--open');
+  });
+
+  $('.menu a').on('click', function (event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 1500);
+  });
+
+  $(window).on('scroll', function (){
+    var height = $(window).scrollTop();
+    if (height > 150){
+      $('.menu').addClass('menu--scroll');
+    }
+    else{
+      $('.menu').removeClass('menu--scroll');
+    }
   });
   
 });
