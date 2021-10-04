038f9ef3ee8a12777848ccc27159cc895c5fb008
mountain-climber <doublem.jquery@gmail.com>
2021-10-04 11:31:43 +0300

    this is ready

--- app/js/main.js
+++ app/js/main.js
@@ -1,24 +1,56 @@
-$(function () {
+document.addEventListener("DOMContentLoaded", function () {
 
-  $('.header__btn, .menu a').on('click', function () {
-    $('.menu').toggleClass('menu--open');
-  });
-
-  $('.menu a').on('click', function (event) {
+  function hrefs(event) {
     event.preventDefault();
     var id = $(this).attr('href'),
       top = $(id).offset().top;
     $('body,html').animate({ scrollTop: top }, 1500);
-  });
+  }
 
-  $(window).on('scroll', function (){
-    var height = $(window).scrollTop();
-    if (height > 150){
-      $('.menu').addClass('menu--scroll');
+  var menuLink = document.querySelectorAll('.menu__list-link[href*="#"]');
+
+  menuLink.forEach(menuLink => { menuLink.addEventListener('click', hrefs) });
+
+  // When the user scrolls the page, execute myFunction
+  window.onscroll = function () { fixedHeader() };
+
+  // Get the header
+  var header = document.querySelector('.menu');
+  var headerScroll = 'menu--scroll';
+
+  // Get the offset position of the navbar
+  var sticky = header.offsetTop;
+
+  // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+  var headerBtn = document.querySelector('.header__btn');
+  var headerBtnScroll = 'header__btn--scroll';
+  function fixedHeader(menuTop) {
+    if (window.pageYOffset >= sticky) {
+      header.classList.add(headerScroll);
+      headerBtn.classList.add(headerBtnScroll);
+
+      menuTop.style.top = '50px';
+    } else {
+      header.classList.remove(headerScroll);
+      headerBtn.classList.remove(headerBtnScroll);
+
+      menuTop.style.top = '25px';
     }
-    else{
-      $('.menu').removeClass('menu--scroll');
-    }
-  });
-  
+  }
+
+  if (window.innerWidth < 893) {
+    var header = document.querySelector('.header__btn-wrap');
+    var headerScroll = 'header__btn-wrap--scroll';
+    window.onscroll = function () { fixedHeader(document.querySelector('.menu')) };
+
+    var menu = document.querySelectorAll('.header__btn, .menu__list-link');
+
+    function menuFunc() {
+      document.querySelector('.menu').classList.toggle('menu--open');
+      document.querySelector('.header__btn').classList.toggle('header__btn--active');
+    };
+
+    menu.forEach(menu => { menu.addEventListener('click', menuFunc) });
+  }
+
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,20 @@
-const { src, dest, watch, parallel, series }  = require('gulp');
+const { src, dest, watch, parallel, series } = require('gulp');
 
-const scss          = require('gulp-sass');
-const concat        = require('gulp-concat');
-const browserSync   = require('browser-sync').create();
-const uglify        = require('gulp-uglify-es').default;
-const autoprefixer  = require('gulp-autoprefixer');
-const imagemin      = require('gulp-imagemin');
-const del           = require('del');
+const scss = require('gulp-sass');
+const concat = require('gulp-concat');
+const browserSync = require('browser-sync').create();
+const uglify = require('gulp-uglify-es').default;
+const autoprefixer = require('gulp-autoprefixer');
+const imagemin = require('gulp-imagemin');
+const imageminPngquant = require('imagemin-pngquant');
+const del = require('del');
 
 function browsersync() {
   browserSync.init({
-    server : {
+    server: {
       baseDir: 'app/'
-    }
+    },
+    notify: false
   });
 }
 
@@ -22,25 +24,26 @@ function cleanDist() {
 
 function images() {
   return src('app/images/**/*')
-    .pipe(imagemin(
-      [
-        imagemin.gifsicle({ interlaced: true }),
-        imagemin.mozjpeg({ quality: 75, progressive: true }),
-        imagemin.optipng({ optimizationLevel: 5 }),
-        imagemin.svgo({
-          plugins: [
-            { removeViewBox: true },
-            { cleanupIDs: false }
-          ]
-        })
-      ]
-    ))
+    .pipe(imagemin([
+      imageminPngquant(),
+      imagemin.gifsicle({ interlaced: true }),
+      imagemin.mozjpeg({ quality: 75, progressive: true }),
+      imagemin.svgo({
+        plugins: [
+          { removeViewBox: true },
+          { cleanupIDs: false }
+        ]
+      })
+    ], {
+      verbose: true
+    }))
     .pipe(dest('dist/images'))
 }
 
 function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
+    'lazyestload/src/js/lazyload.js',
     'app/js/main.js'
   ])
     .pipe(concat('main.min.js'))
@@ -52,14 +55,14 @@ function scripts() {
 
 function styles() {
   return src('app/scss/style.scss')
-      .pipe(scss({outputStyle: 'compressed'}))
-      .pipe(concat('style.min.css'))
-      .pipe(autoprefixer({
-        overrideBrowserslist: ['last 10 version'],
-        grid: true
-      }))
-      .pipe(dest('app/css'))
-      .pipe(browserSync.stream())
+    .pipe(scss({ outputStyle: 'compressed' }))
+    .pipe(concat('style.min.css'))
+    .pipe(autoprefixer({
+      overrideBrowserslist: ['last 10 version'],
+      grid: true
+    }))
+    .pipe(dest('app/css'))
+    .pipe(browserSync.stream())
 }
 
 function build() {
@@ -68,7 +71,7 @@ function build() {
     'app/fonts/**/*',
     'app/js/main.min.js',
     'app/*.html'
-  ], {base: 'app'})
+  ], { base: 'app' })
     .pipe(dest('dist'))
 }
 
@@ -88,5 +91,3 @@ exports.cleanDist = cleanDist;
 
 exports.build = series(cleanDist, images, build);
 exports.default = parallel(styles, scripts, browsersync, watching);
-
-
